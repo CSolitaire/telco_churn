93084547255c4a15652a4b056234e17ba99152eb
Corey Solitaire <corey.solitaire@gmail.com>
2020-09-18 16:54:55 -0500

    Modified cat variable

--- prepare.py
+++ prepare.py
@@ -24,22 +24,29 @@ def prep_telco_data(df):
     df.phone_service.replace(['Yes', 'No'], [1,0], inplace = True)
     df.paperless_billing.replace(['Yes', 'No'], [1,0], inplace = True)
     ## Add dummy variables as new columns in dataframe and rename them, delete origional
+    df["gender_cc"] = df["gender"].astype('category')
+    df["gender_cc"] = df["gender_cc"].cat.codes
     #gender = df.gender.str.get_dummies()
     #df = pd.concat([df, gender], axis=1)
     #df.rename(columns = {'Female': 'is_female', 'Male': 'is_male'}, inplace = True)
     #df.drop(columns = ['gender'], inplace = True)
     ## Add dummy variables as new columns in dataframe and rename them, delete origional
     #multiple = df.multiple_lines.str.get_dummies()
+    df["multiple_lines_cc"] = df["multiple_lines"].astype('category')
+    df["multiple_lines_cc"] = df["multiple_lines_cc"].cat.codes
     #df = pd.concat([df, multiple], axis=1)
     #df.rename(columns = {'No': 'no_multiple_lines', 'Yes': 'yes_multiple_lines'}, inplace = True)
     #df.drop(columns = ['multiple_lines'], inplace = True)
     ## Add dummy variables as new columns in dataframe and rename them, delete origional
     #multiple = df.online_security.str.get_dummies()
+    df["online_security_cc"] = df["online_security"].astype('category')
+    df["nline_security_cc"] = df["nline_security_cc"].cat.codes
     #df = pd.concat([df, multiple], axis=1)
     #df.rename(columns = {'No': 'no_online_security', 'Yes': 'yes_online_security'}, inplace = True)
     #df.drop(columns = ['online_security'], inplace = True)
     ## Add dummy variables as new columns in dataframe and rename them, delete origional
     #multiple = df.online_backup.str.get_dummies()
+    
     #df = pd.concat([df, multiple], axis=1)
     #df.rename(columns = {'No': 'no_online_backup', 'Yes': 'yes_online_backup'}, inplace = True)
     #df.drop(columns = ['online_backup'], inplace = True)
